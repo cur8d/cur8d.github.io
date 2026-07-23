aa414e2e2a96d98b6b7f4eb355a9a832c1550150
Amr Abed <3361565+amrabed@users.noreply.github.com>
2026-07-23 07:41:25 -0400

    feat: add lambda route to sitemap entries

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,5 +23,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/lambda`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
